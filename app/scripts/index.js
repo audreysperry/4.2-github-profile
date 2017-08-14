779b8dadecc03ff46d7161e7ef497657de1c5004
Audrey Sperry <audrey.sperry@gmail.com>
2017-08-14 13:36:27 -0400

    styling

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,64 +1,83 @@
 let $ = require('jquery');
 let handlebars = require('handlebars');
 let token = require('./githubtoken.js').token;
+let moment = require('moment');
 
 
 
 init();
 
+//display account nav on page
+function displayAccountNav(user) {
+  let $repoNav = $('.inner-nav');
+  console.log($repoNav);
+  let source = $('#repo-nav-template').html();
+  console.log(source);
+  let template = handlebars.compile(source);
 
+  let $repoNavInfo = $(template(user));
+  $repoNav.append($repoNavInfo);
+}
+
+
+//display user profile info on left side of page
 function displayUserInfo(user){
-  var $userProfile = $('#user-profile-info');
-  var source = $('#user-profile-template').html();
-  var template = handlebars.compile(source);
+  let $userProfile = $('#user-profile-info');
+  let source = $('#user-profile-template').html();
+  let template = handlebars.compile(source);
 
-    var $userInfo = $(template(user));
+    let $userInfo = $(template(user));
     $userProfile.append($userInfo);
 }
 
+//display small profile picture in navigation bar
 function displayMiniPhoto(user){
-  var $repoNav = $('#repo-photo');
-  var source = $('#repo-photo-template').html();
-  var template = handlebars.compile(source);
+  let $repoNav = $('#repo-photo');
+  let source = $('#repo-photo-template').html();
+  let template = handlebars.compile(source);
 
-  var $miniPhoto = $(template(user));
+  let $miniPhoto = $(template(user));
   $repoNav.append($miniPhoto);
 }
-
+//display repo info on page
 function displayRepoInfo(repos){
-  var $repoList = $('#repo-info');
-  var source = $('#repo-template').html();
-  var template = handlebars.compile(source);
-
-  console.log(repos);
+  let $repoList = $('#repo-info');
+  let source = $('#repo-template').html();
+  let template = handlebars.compile(source);
 
   repos.forEach(function(repo){
-    var $reposInfo = $(template(repo));
+    repo.updated_at = moment(repo.updated_at).fromNow();
+    let $reposInfo = $(template(repo));
+
+    // $reposInfo.find('.lang-color').addClass(repo.language);
     $repoList.append($reposInfo);
   });
 
 }
-var options = {
+//add github token to authorize api requests
+const currentToken = token ? 'token ' + token : '';
+const options = {
   headers: {
-    'Authorization': 'token' + token
-  }
-};
+    'Authorization': currentToken,
+    'Accept': 'application/vnd.github.mercy-preview+json'
 
+  }
+
+};
+//initiate the fetch requests to github
 function init(){
 
-  var options = {};
   fetch('https://api.github.com/users/audreysperry', options).then(function(response){
     return response.json();
   }).then(function(data){
-    console.log(data);
     displayUserInfo(data);
     displayMiniPhoto(data);
+    displayAccountNav(data);
   });
 
   fetch('https://api.github.com/users/audreysperry/repos', options).then(function(response){
     return response.json();
   }).then(function(data){
-    console.log(data);
     displayRepoInfo(data);
   });
 
